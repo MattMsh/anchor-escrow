78a7aaa5fc7939fce5b366d507465b57cc61caac
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2025-07-10 20:12:55 +0300

    init

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,33 @@
+#![allow(unexpected_cfgs)]
+
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("xTCiPDvXpEVVP4dfaGpCmwX5mgM7eektauzuBbdCMBD");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
+}
